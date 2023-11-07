0719233d26fe355a996249d1839311fb598c4611
Luis <ddajps.20202@gmail.com>
2023-11-06 18:36:23 -0600

    Código base

--- src/main/java/mx/unitec/Controlador.java
+++ src/main/java/mx/unitec/Controlador.java
@@ -2,11 +2,25 @@ package mx.unitec;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Controlador {
 
     @Autowired
     private UsuarioDao usuarioDao;
+
+    @RequestMapping("/home")
+    public ModelAndView home() {
+        return new ModelAndView("home");
+    }
+
+    @RequestMapping(value = "/guardarUsuario", method = RequestMethod.POST)
+    public ModelAndView guardarUsuario() {
+        return new ModelAndView("guardarUsuario");
+    }
+
 
 }
--- src/main/java/mx/unitec/Usuario.java
+++ src/main/java/mx/unitec/Usuario.java
@@ -1,4 +1,12 @@
 package mx.unitec;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "docente_usuarios")
 public class Usuario {
+    @Id  // @Id especifica que este campo es la llave primaria
+    private int idUsuario;
 }
